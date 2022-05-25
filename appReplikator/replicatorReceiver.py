d3c35e5334506e0c9bf951fc09a25a250072bc80
MilanTo3 <mtomin367@gmail.com>
2022-05-26 01:34:40 +0200

    Start a replicator receiver service.

---
+++ appReplikator/replicatorReceiver.py
@@ -0,0 +1,27 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 5052
+SERVER = 'localhost'
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+shotServer = "localhost"
+shotPort = 5053
+
+def handle_client(conn, addr):
+    print(f"[NEW CONNECTION] {addr} connected.")
+                
+                    
+if __name__ == "__main__": # pragma: no cover
+    
+    listEl = []
+    
+    print("[STARTING] server is starting...")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    server.listen()
+    print(f"[LISTENING] Server is listening on {SERVER}")
+    conn, addr = server.accept()
+    print(f"Replicator sender accepted.")
+    handle_client(conn, addr)
