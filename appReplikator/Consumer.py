ecc1057e9cc00d22edb088810da88e4dbebd7211
Bojan <boksaue.jakovljevic@gmail.com>
2022-06-08 17:40:47 +0200

    Class Consumer created

---
+++ appReplikator/Consumer.py
@@ -0,0 +1,10 @@
+class Consumer:
+
+     def __init__(self, id, name, surname, street, s_num, post_num, city):
+        self.id = id
+        self.name = name
+        self.surname = surname
+        self.street = street
+        self.s_num = s_num
+        self.post_num = post_num
+        self.city = city
